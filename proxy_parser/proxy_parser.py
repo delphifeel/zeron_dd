7b03a06e67338ec38b68adecda8fb2654b9be3c5
delphifeel <delphifeel@gmail.com>
2022-03-03 19:28:55 +0200

    init

---
+++ proxy_parser/proxy_parser.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def main():
+	response = requests.get("https://gitlab.com/cto.endel/atack_hosts/-/raw/master/hosts.json")
+	api_list = response.json()
+	proxy_file_content = ""
+	for api in api_list:
+		api_json = None
+		try:
+			api_resp = requests.get(api, timeout=2)
+			api_json = api_resp.json()
+		except:
+			print("%s UNAVAILABLE" % (api))
+			continue
+
+		for proxy in api_json["proxy"]:
+			proxy_file_content += proxy["ip"].strip() + "|" + proxy["auth"].strip() + "\n"
+
+
+	with open("proxies.txt", "w") as f:
+		f.write(proxy_file_content)
+
+main()
